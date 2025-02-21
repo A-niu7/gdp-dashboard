e9e82b122197c5e372680ef78965291dc28c899a
A-niu7 <57707024+A-niu7@users.noreply.github.com>
2025-02-21 03:14:14 +0000

    hello world 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard Hello World',
+    page_title='GDP dashboard Hello World 3',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
+
+
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
