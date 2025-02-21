4912c09fce8625670ac3ceeea3f9cff8f975667d
A-niu7 <57707024+A-niu7@users.noreply.github.com>
2025-02-21 03:27:12 +0000

    table2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write("Here's our first attempt at using data to create a table:")
 test_df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
-}
-st.write(st.table(
-    test_df
-)))
+})
+st.write(
+    st.table(test_df)
+)
