2d54b91fd2a4b37433f47870ee12eaeedc04a0fc
A-niu7 <57707024+A-niu7@users.noreply.github.com>
2025-02-21 03:37:09 +0000

    buttor

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ import numpy as np
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
